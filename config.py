83c9206f600d94cd66ead8631a8f3a09fbfefb19
Nisoka <yaramnisoka@gmail.com>
2025-09-28 19:14:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = '8bd5d300ff4320cc496799952ac9dd6691d5a0d3aec0b291'
+    SECRET_KEY = ''
     SQLALCHEMY_DATABASE_URI ='sqlite:///parking.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/uploads'
+
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max file size
